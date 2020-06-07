e1756dc6c41f5e479683aab67bdaa35bb1290319
Ulises Santana <ulises.santana@netexlearning.net>
2020-06-07 03:24:23 +0100

    :lipstick: improve styles

--- src/app/views/ToggleDashboard.style.tsx
+++ src/app/views/ToggleDashboard.style.tsx
@@ -1,15 +1,12 @@
 import styled from 'styled-components'
 
 export default styled.main`
-  background-color: rgba(27, 27, 27, 0.98);
+  background-color: rgba(27, 27, 27, 1);
   color: ghostwhite;
   display: flex;
   flex-wrap: wrap;
   font-size: 100%;
   height: calc(100% - 72px);
-  //margin: 0 auto;
-  min-height: calc(100% - 72px);
-  min-width: calc(100% - 72px);
   padding: 36px;
   width: calc(100% - 72px);
 `
