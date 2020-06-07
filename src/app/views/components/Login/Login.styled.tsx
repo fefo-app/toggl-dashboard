86a4d7103b5debfa4ad628710e253c470cab33a9
Ulises Santana <ulises.santana@netexlearning.net>
2020-06-07 02:47:03 +0100

    :lipstick: improve styles for login view

--- src/app/views/components/Login/Login.styled.tsx
+++ src/app/views/components/Login/Login.styled.tsx
@@ -1,14 +1,37 @@
 import styled from 'styled-components'
 
-export default styled.div`
-  position: relative;
+export default styled.section`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 50vh;
+  justify-content: center;
+  width: 100%;
 
-  button {
-    background-color: transparent;
-    border: none;
-    color: white;
-    position: fixed;
-    right: 4px;
-    top: 8px;
+  img {
+    width: 64px;
+    margin-bottom: 24px;
+  }
+
+  form {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    fieldset {
+      align-items: center;
+      border: none;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      justify-content: center;
+    }
+
+    fieldset > input,
+    button {
+      margin: 8px 0;
+      padding: 8px;
+    }
   }
 `
--- src/app/views/components/Login/Login.tsx
+++ src/app/views/components/Login/Login.tsx
@@ -1,29 +1,37 @@
 import React, { FormEvent, Dispatch } from 'react'
 import { loginDispatcher, TogglAction } from '../../../store/toggl'
+import StyleWrapper from './Login.styled'
 
-export function Login({dispatch}: {dispatch: Dispatch<TogglAction>}) {
-
+export function Login({ dispatch }: { dispatch: Dispatch<TogglAction> }) {
   const onSubmit = (event: FormEvent) => {
     event.preventDefault()
     const form = event.target as HTMLFormElement
-    const username = form.username.value;
-    const password = form.password.value;
+    const username = form.username.value
+    const password = form.password.value
     loginDispatcher(dispatch)(username, password)
-  };
+  }
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        name="username"
-        placeholder={"Email que usas en toggl"}
-        type="text"
+    <StyleWrapper>
+      <img
+        src='https://toggl.com/common/images/share/favicon/favicon-192x192-29519727140c0d39a540787154c03a2f.png'
+        alt='Toggl Logo'
       />
-      <input
-        name="password"
-        placeholder={"Contraseña"}
-        type="password"
-      />
-      <button type="submit">Login</button>
-    </form>
-  );
+      <form onSubmit={onSubmit}>
+        <fieldset>
+          <label htmlFor='username'>Email:</label>
+          <input
+            name='username'
+            placeholder='Email que usas en toggl'
+            type='text'
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor='username'>Contraseña:</label>
+          <input name='password' placeholder='Contraseña' type='password' />
+        </fieldset>
+        <button type='submit'>Login</button>
+      </form>
+    </StyleWrapper>
+  )
 }
